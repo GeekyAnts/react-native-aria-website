45016c7bad5df95debf0e0d2ba61411733be7ec0
Ankur Kedia <theankurkedia@gmail.com>
2021-02-22 18:54:46 +0530

    fixes for config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,72 +1,72 @@
 module.exports = {
-  title: "React Native ARIA",
+  title: 'React Native ARIA',
   tagline:
-    "React Native ARIA handles behaviour, accessibility and interactions so you can implement custom components and design systems for Android, iOS and Web",
-  url: "https://geekyants.github.io",
-  baseUrl: "/react-native-aria/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "geekyants", // Usually your GitHub org/user name.
-  projectName: "react-native-aria", // Usually your repo name.
+    'React Native ARIA handles behaviour, accessibility and interactions so you can implement custom components and design systems for Android, iOS and Web',
+  url: 'https://react-native-aria.geekyants.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'geekyants', // Usually your GitHub org/user name.
+  projectName: 'react-native-aria', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: "React Native ARIA",
+      title: 'React Native ARIA',
       logo: {
-        alt: "My Site Logo",
-        src: "img/logo.png",
+        alt: 'My Site Logo',
+        src: 'img/logo.png',
       },
       items: [
         {
-          to: "docs/",
-          activeBasePath: "docs",
-          label: "Docs",
-          position: "right",
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'right',
         },
         {
-          href: "https://github.com/GeekyAnts/react-native-aria",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/GeekyAnts/react-native-aria',
+          label: 'GitHub',
+          position: 'right',
         },
         {
           href:
-            "https://geekyants.com/hire?utm_source=reactnativearia&utm_medium=header&utm_campaign=reactnativearia",
-          label: "Hire Us",
-          position: "right",
+            'https://geekyants.com/hire?utm_source=reactnativearia&utm_medium=header&utm_campaign=reactnativearia',
+          label: 'Hire Us',
+          position: 'right',
         },
       ],
     },
     footer: {
-      style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Docs",
+          title: 'Docs',
           items: [
             {
-              label: "Getting Started",
-              to: "docs/",
+              label: 'Getting Started',
+              to: 'docs/',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
             {
-              label: "GitHub",
-              href: "https://github.com/GeekyAnts/react-native-aria",
+              label: 'GitHub',
+              href: 'https://github.com/GeekyAnts/react-native-aria',
             },
             {
-              label: "Roadmap",
-              href: "https://trello.com/b/8HcLFH3K/react-native-aria-roadmap",
+              label: 'Roadmap',
+              href: 'https://trello.com/b/8HcLFH3K/react-native-aria-roadmap',
             },
           ],
         },
         {
-          title: " ",
+          title: ' ',
           items: [
             {
-              label: "Built with ❤️ by GeekyAnts ",
-              href: "https://www.geekyants.com",
+              label: 'Built with ❤️ by GeekyAnts ',
+              href: 'https://www.geekyants.com',
             },
           ],
         },
@@ -75,16 +75,16 @@ module.exports = {
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/master/website/",
+            'https://github.com/facebook/docusaurus/edit/master/website/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
